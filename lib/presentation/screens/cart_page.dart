ae8e4373bc206eb9cda00c461d6d2d53a07caaba
kidlinux7 <princeherman08@gmail.com>
2022-03-20 19:05:25 +0300

    Flutterwave intergration

--- lib/presentation/screens/cart_page.dart
+++ lib/presentation/screens/cart_page.dart
@@ -1,5 +1,12 @@
+import 'dart:ffi';
+import 'dart:io';
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutterwave/flutterwave.dart';
+// import 'package:flutterwave/core/flutterwave.dart';
+import 'package:flutterwave/models/responses/charge_response.dart';
 import 'package:megastore/logic/bloc/cart_bloc.dart';
 
 class CartPage extends StatefulWidget {
@@ -12,11 +19,29 @@ class CartPage extends StatefulWidget {
 class _CartPageState extends State<CartPage> {
   late final CartBloc cartBloc;
   double totalValue = 0;
+  String refKey = '';
+  String checkoutTotal = '';
+
+// Acts like a unique ID (This value should be generated from the server)
+  void setRef() {
+    Random rand = Random();
+    int number = rand.nextInt(2000);
+    if (Platform.isAndroid) {
+      setState(() {
+        refKey = "AndroidRef12433$number";
+      });
+    } else {
+      setState(() {
+        refKey = "iOSRef14553$number";
+      });
+    }
+  }
 
   @override
   void initState() {
     cartBloc = BlocProvider.of<CartBloc>(context);
     // cartTotalPrice();
+    setRef();
     super.initState();
   }
 
@@ -76,6 +101,7 @@ class _CartPageState extends State<CartPage> {
             for (int x = 0; x < state.cartproducts.length; x++) {
               totalValue += (state.cartproducts[x].price *
                   state.cartproducts[x].quantity);
+              checkoutTotal = (totalValue).toString();
             }
 
             return Column(
@@ -162,45 +188,79 @@ class _CartPageState extends State<CartPage> {
                         ),
                       ));
                     }),
-                state.cartproducts == null || state.cartproducts.length == 0
-                    ? Center()
-                    : Padding(
-                        padding: const EdgeInsets.all(15.0),
-                        child: InkWell(
-                          // onTap: cartTotalPrice,
-                          child: Container(
-                            width: deviceWidth,
-                            height: 50.0,
-                            decoration: BoxDecoration(
-                              color: Colors.black,
-                              borderRadius: BorderRadius.circular(10),
-                            ),
-                            child: Center(
-                              child: Padding(
-                                padding: const EdgeInsets.symmetric(
-                                    horizontal: 14.0),
-                                child: Row(
-                                  mainAxisAlignment:
-                                      MainAxisAlignment.spaceBetween,
-                                  children: [
-                                    Text(
-                                      'Total',
-                                      style: TextStyle(color: Colors.white),
-                                    ),
-                                    Text(
-                                      "$totalValue",
-                                      textScaleFactor: 1.7,
-                                      style: TextStyle(
-                                          color: Colors.white,
-                                          fontWeight: FontWeight.w900),
-                                    ),
-                                  ],
-                                ),
+                Padding(
+                  padding: const EdgeInsets.all(15.0),
+                  child: InkWell(
+                    // onTap: cartTotalPrice,
+                    child: Container(
+                      width: deviceWidth,
+                      height: 50.0,
+                      decoration: BoxDecoration(
+                        color: Colors.black,
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                      child: Center(
+                        child: Padding(
+                          padding: const EdgeInsets.symmetric(horizontal: 14.0),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Text(
+                                'Total',
+                                style: TextStyle(color: Colors.white),
                               ),
-                            ),
+                              Text(
+                                "$totalValue",
+                                textScaleFactor: 1.7,
+                                style: TextStyle(
+                                    color: Colors.white,
+                                    fontWeight: FontWeight.w900),
+                              ),
+                            ],
                           ),
                         ),
                       ),
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(15.0),
+                  child: InkWell(
+                    onTap: (() {
+                      chechout(context, checkoutTotal);
+                    }),
+                    child: Container(
+                      width: deviceWidth,
+                      height: 50.0,
+                      decoration: BoxDecoration(
+                        color: Color.fromARGB(255, 151, 151, 151),
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                      child: Center(
+                        child: Padding(
+                          padding: const EdgeInsets.symmetric(horizontal: 14.0),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Text(
+                                "Checkout",
+                                textScaleFactor: 1.3,
+                                style: TextStyle(
+                                    color: Colors.white,
+                                    fontWeight: FontWeight.w900),
+                              ),
+                              Icon(
+                                Icons.chevron_right_rounded,
+                                color: Colors.white,
+                                size: 25.0,
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
               ],
             );
           } else {
@@ -209,5 +269,56 @@ class _CartPageState extends State<CartPage> {
         },
       ),
     );
+  }
+
+  chechout(BuildContext context, String checkoutTotal) async {
+    try {
+      Flutterwave flutterwave = Flutterwave.forUIPayment(
+          context: this.context,
+          encryptionKey: "FLWSECK_TEST45496b6b4694",
+          publicKey: "FLWPUBK_TEST-44f95e3900d8a763f7154fd624fc65b3-X",
+          currency: "NGN",
+          amount: checkoutTotal,
+          email: "princeherman08@gmail.com",
+          fullName: "Prince Herman Mutungi",
+          txRef: refKey,
+          isDebugMode: true,
+          phoneNumber: "+255716241549",
+          acceptCardPayment: true,
+          acceptUSSDPayment: false,
+          acceptAccountPayment: false,
+          acceptFrancophoneMobileMoney: false,
+          acceptGhanaPayment: false,
+          acceptMpesaPayment: true,
+          acceptRwandaMoneyPayment: false,
+          acceptUgandaPayment: false,
+          acceptZambiaPayment: false);
+
+      final ChargeResponse response =
+          await flutterwave.initializeForUiPayments();
+
+      if (response == null) {
+        // print(response.message);
+        // print(response.status);
+        print('Transaction Failed');
+        ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+          content: Text("Transaction was not successful"),
+          backgroundColor: Colors.red,
+        ));
+      } else {
+        if (response.status == 'success') {
+          print(response.message);
+          print(response.status);
+          ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+            content: Text("Transaction was successful"),
+            backgroundColor: Colors.green,
+          ));
+        }
+      }
+    } catch (error) {
+      print(error);
+      ScaffoldMessenger.of(context)
+          .showSnackBar(const SnackBar(content: Text("Something went wrong")));
+    }
   }
 }
